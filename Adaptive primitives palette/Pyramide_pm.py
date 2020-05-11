6b0f626a6fd97d0f8af0007a43eeab032e5e4484
sergionic <sergionic78@gmail.com>
2020-05-11 21:46:13 +0300

    First release

---
+++ Adaptive primitives palette/Pyramide_pm.py
@@ -0,0 +1,17 @@
+import c4d, os
+
+def main():
+    
+    fn = "preset://adaptive primitives.lib4d/pyramide" #.c4d"
+
+    doc.StartUndo() # Start recording undos
+    flags = c4d.SCENEFILTER_OBJECTS | c4d.SCENEFILTER_MATERIALS | c4d.SCENEFILTER_MERGESCENE # Merge objects and materials
+    
+    c4d.documents.MergeDocument(doc, fn, flags)
+    
+    c4d.EventAdd() # Refresh Cinema 4D
+    doc.EndUndo() # Stop recording undos
+
+    
+if __name__=='__main__':
+    main()
